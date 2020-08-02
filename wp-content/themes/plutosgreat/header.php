9ec39ba219f11a59482c5e984cca1401e4e7ba47
Sergey Smolanov <smolanovsergey@gmail.com>
2020-08-02 20:23:15 +0300

    мобайл

--- wp-content/themes/plutosgreat/header.php
+++ wp-content/themes/plutosgreat/header.php
@@ -19,6 +19,8 @@
         <link rel="stylesheet" type="text/css" href="<?php get_template_directory_uri()?> . /slick/slick.css"/>
         <link rel="stylesheet" type="text/css" href="<?php get_template_directory_uri()?> ./slick/slick-theme.css"/>
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans&family=Raleway:wght@300&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" href="/icon.ico" type="image/x-icon">
+        <link rel="icon" type="image/png" href="/icon.png">
             <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
             <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
         <!--    <script type="text/javascript" src="--><?php //get_template_directory_uri()?><!-- ./src/js/slick.js"></script>-->
